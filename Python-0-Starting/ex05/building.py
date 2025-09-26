74fb2fa29518b1fb72cc760453a09a9337ae2cb1
aurxf <auref.contact@protonmail.com>
2025-09-26 04:56:31 +0200

    flake8 fixed

--- Python-0-Starting/ex05/building.py
+++ Python-0-Starting/ex05/building.py
@@ -37,7 +37,7 @@ def main():
         elif len(sys.argv) == 2:
             text = sys.argv[1]
         else:
-            raise AssertionError("AssertionError: more than 1 argument provided")
+            raise AssertionError("AssertionError: The argument are bad")
         count_str_and_print(text)
     except Exception as e:
         print(e)
