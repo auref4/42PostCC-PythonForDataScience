a9793944c4628edfa674a4b1b3f227814a941208
aurxf <auref.contact@protonmail.com>
2025-09-26 04:51:02 +0200

    comment fixed

--- Python-1-Array/ex00/give_bmi.py
+++ Python-1-Array/ex00/give_bmi.py
@@ -5,7 +5,7 @@ def give_bmi(height: list[int | float],
              weight: list[int | float]) -> list[int | float]:
     """Use array to use numpy, calcul bmi and return result into list"""
 
-    # Security
+    # Input validation
     if not isinstance(height, list) or not isinstance(weight, list):
         raise TypeError("Parameters must be lists")
     if not all(isinstance(v, (int, float)) for v in height):
@@ -26,13 +26,13 @@ def give_bmi(height: list[int | float],
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """Use array to use numpy, check if bmi > limit and return bool list"""
 
-    # Security
+    # Input validation
     if not isinstance(bmi, list) or not isinstance(limit, int):
         raise TypeError("Parameters must be one list and one int")
     if not all(isinstance(v, (int, float)) for v in bmi):
         raise TypeError("The values in bmi must be int or float")
 
-    #  Apply limit
+    # Apply limit
     np_bmi = np.array(bmi)
     result = np_bmi > limit  # Compare bmi with limit and return bool array
 
